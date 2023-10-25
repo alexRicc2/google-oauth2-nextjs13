1a92ec2fa82e27aae054d1e1ae6c36bf58a413c6
tanselle <tanselle@student.21-school.ru>
2023-10-26 01:18:17 +0600

    fixed bug

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,13 +1,18 @@
-import GoogleProvider from 'next-auth/providers/google'
+// pages/api/auth/[...nextauth].js
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   providers: [
-    GoogleProvider(
-      {
-        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
-      }
-    )
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: 'https://www.googleapis.com/auth/webmasters.readonly https://www.googleapis.com/auth/analytics.readonly',
+          prompt: "consent", // Это заставит пользователя подтвердить доступ даже если он уже предоставлен
+        },
+      },
+    }),
   ],
-  secret: 'gfghmzzfdgfhmhmvkj' //can be anything
+  secret: 'sdfsdfsdfsdfsdf',
 }
